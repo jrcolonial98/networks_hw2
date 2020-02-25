5d081ebc936911c208857dd2c0203b9aa3497a11
Brendan Gregory <brendangregory@Brendans-MacBook-Pro.local>
2020-02-25 18:41:08 -0500

    second bullet point done

--- p3.py
+++ p3.py
@@ -17,5 +17,35 @@ def createDirGraph(adj):
 				DG.add_edge(i, j)
 	nx.draw(DG)
 	plt.show()
+	return DG
 
-createDirGraph([[0,1,1],[0,0,1],[0,0,0]])
+def bfs(DG, v):
+	# initialize
+	S = {v}
+	visited = set()
+	to_visit = [v]
+
+	while len(to_visit) > 0:
+		# queue: grab first item
+		node = to_visit[0]
+		to_visit = to_visit[1:]
+		visited.add(node)
+
+		# for all neighbors of node
+		for neighbor in DG.neighbors(node):
+			# do not repeat nodes
+			if not neighbor in visited:
+				to_visit.append(neighbor)
+				S.add(neighbor)
+
+	# return S
+	return S
+
+
+
+print("displaying graph:")
+DG = createDirGraph([[0,1,1],[0,0,1],[0,0,0]])
+
+print("Using BFS to find all nodes reachable from node 0:")
+S = bfs(DG, 0)
+print(S)
